bbe802ed0544bfa8e271e55aac21ac3ada5d9c9a
Zhu Liu <zhuliu@Zhus-MacBook-Pro.local>
2018-11-06 06:17:03 -0800

    add example

---
+++ config.py
@@ -0,0 +1,6 @@
+HOST = "http://206.189.218.218:5000/api"
+HEADER_INDEX = "config_data/header_index.json"
+HEADER_DESP = 'config_data/annotated.snp.description.txt'
+SKIP_ITEMS = [ "ID", "QUAL", "FILTER", "INFO","alt_hg19", "GTEx_V6_tissue", "chr_hg19", "pos_hg19", "ref_hg19=ref_hg38", "ref_hg19", "GTEx_V6_gene"]
+GENE_REGION_FILE = "config_data/hg18_symbol_enst_pos.json"
+HEADER_BASIC = [0,1,3,4]
